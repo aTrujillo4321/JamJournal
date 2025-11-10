2816ea75d693da91f1ad9820e754786edc20f6d8
aTrujillo4321 <aletrujillo@csumb.edu>
2025-11-10 15:00:42 -0800

    initial commit: JamJournal

---
+++ index.mjs
@@ -0,0 +1,45 @@
+import express from 'express';
+import mysql from 'mysql2/promise';
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));
+
+const pool = mysql.createPool({
+    host: "",
+    user: "",
+    password: "",
+    database: "",
+    connectionLimit: 10,
+    waitForConnections: true
+});
+
+app.get('/', (req, res) => {
+   res.render('login.ejs')
+});
+
+app.get('/signup', (req, res) => {
+    res.render('signup.ejs')
+});
+
+app.get('/library', (req, res) => {
+    res.render('library.ejs')
+});
+
+app.get('/adding', (req, res) => {
+    res.render('adding.ejs')
+});
+
+app.get('/searching', (req, res) => {
+    res.render('searching.ejs')
+});
+
+app.get('/deleting', (req, res) => {
+    res.render('deleting.ejs')
+});
+
+app.listen(3000, ()=>{
+    console.log("Express server running")
+});
